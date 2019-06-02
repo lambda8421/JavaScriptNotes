12a271c26e368561c17f4f81feb70682ae25f5aa
lambda8421 <lambda8421@163.com>
2019-06-01 21:22:40 -0400

    Add test

--- algorithms/leetcode/Main.js
+++ algorithms/leetcode/Main.js
@@ -1,5 +1,14 @@
 process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
 
-  console.log('test')
+  console.log('label' + val + ' ' + index  + ' ' + array);
+
+  console.time('testtime')
+
+  for (let i = 0; i < 1000; i++) {
+    for (let j = 0; j < 10000; j++) {
+      const x = 1;
+    }
+  }
+
+  console.timeEnd('testtime')
 });
