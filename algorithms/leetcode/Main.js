7a8ea29df63fbbcfb5dde819f926f0aa532f95bc
yongzhao <lambda8421@163.com>
2019-06-05 14:30:26 -0400

    Add changes

--- algorithms/leetcode/Main.js
+++ algorithms/leetcode/Main.js
@@ -14,3 +14,16 @@ process.argv.forEach(function (val, index, array) {
 
 
 });
+
+
+function forEach(list, callback) {
+  for (var n = 0; n < list.length; n++) {
+    callback.call(list[n], n);
+  }
+}
+
+var weapons = [{ type: 'shuriken' },{ type: 'katana' },{ type: 'nunchucks' }];
+
+forEach(weapons, function (index) {
+  console.log(this === weapons[index])
+});
