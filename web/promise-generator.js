b7918d73e2bd9f112ffbba75ce816f4a9d59173a
Yongzhao <yongzhao@geniusplaza.com>
2019-06-10 14:35:09 -0400

    Add changes

---
+++ web/promise-generator.js
@@ -0,0 +1,32 @@
+let p1 = new Promise(r=>r('P1'));
+let p2 = new Promise(r=>r('P2'));
+
+combine(function* () {
+  try {
+     yield p1;
+    yield p2;
+  }catch (e) {
+    //something wrong
+  }
+});
+
+
+function combine(generator) {
+  let it = generator();
+
+  function handle(itResult) {
+    if (itResult.done) return;
+    const itValue = itResult.value;
+    console.log(itValue);
+    if (itValue instanceof Promise){
+      itValue.then(res=>handle(it.next(res))
+        .catch(err=>it.throw(err)))
+    }
+  }
+
+  try {
+    handle(it.next());
+  }catch (e) {
+    it.throw(e)
+  }
+}
