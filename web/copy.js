00e7e9124194acee6f2112509d50a0b7d0e5907e
yongzhao <lambda8421@163.com>
2019-06-27 14:39:17 -0400

    Update copy.js

--- web/copy.js
+++ web/copy.js
@@ -27,3 +27,18 @@ console.log(source);
 
 console.log(returnedTarget);
 // expected output: Object { a: 1, b: 4, c: 5 }
+
+
+//deep copy
+let array = [{ a: 'a' }];
+let cloneArray = JSON.parse(JSON.stringify(array))
+
+array[0].a = 'ff';
+
+console.log(array,cloneArray)
+
+let obj = {o:{x:'x'}};
+let obj1 = JSON.parse(JSON.stringify(obj));
+obj.o.x = 'u';
+
+console.log(obj,obj1);
