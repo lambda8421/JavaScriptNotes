e03d3ba456161040523b2597d6dd5ccb9c2c1558
yongzhao <lambda8421@163.com>
2019-06-28 14:15:41 -0400

    Update copy.js

--- web/copy.js
+++ web/copy.js
@@ -52,3 +52,19 @@ nObj.x.y = 'x'
 
 console.log(nObj,nObj1)
 
+
+//deep copy method
+function copy(aObject) {
+  if (!aObject) {
+    return aObject;
+  }
+
+  let v;
+  let bObject = Array.isArray(aObject) ? [] : {};
+  for (const k in aObject) {
+    v = aObject[k];
+    bObject[k] = (typeof v === "object") ? copy(v) : v;
+  }
+
+  return bObject;
+}
