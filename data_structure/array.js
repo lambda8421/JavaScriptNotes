badcef0255c6ad3718a467b794ca69844d87fc7b
yongzhao <lambda8421@163.com>
2019-07-02 14:31:34 -0400

    Update array.js

--- data_structure/array.js
+++ data_structure/array.js
@@ -75,3 +75,15 @@ const some = x=>x>1;
 arr.forEach(forEach);
 console.log(arr.every(every));
 console.log(arr.some(some));
+
+
+
+/*
+The Array.from() method creates a new, 
+shallow-copied Array instance from an array-like or iterable object.
+*/
+console.log(Array.from('foo'));
+// expected output: Array ["f", "o", "o"]
+
+console.log(Array.from([1, 2, 3], x => x + x));
+// expected output: Array [2, 4, 6]
