7b01aff15195f795b0560d0d701341c10102b177
Yongzhao <yongzhao@geniusplaza.com>
2019-07-09 22:02:22 -0400

    Add changes

--- web/string.js
+++ web/string.js
@@ -1,4 +1,12 @@
-const str = 'abcabcabc';
+let str = 'abcabcabc';
 
 console.log(str.split('b'));
 console.log(str.split('b',2));
+
+
+
+str = 'b';
+console.log(str.split('b'))  //['','']
+
+str = 'abbc';
+console.log(str.split('b'))  //['a','','c']
