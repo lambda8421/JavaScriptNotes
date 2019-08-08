bb4de077c1faea652b3364c84ea2ee61daa01786
yongzhao <lambda8421@163.com>
2019-08-08 11:35:41 -0400

    add new question

--- interview/requestStartAndEnd.js
+++ interview/requestStartAndEnd.js
@@ -12,14 +12,32 @@ request2 start: 1, end 5
 request3 start: 2, end 4
 
  */
-
-const state = new Map();
+const startArr = [];
+const endArr = [];
 
 function requestStart(task,start) {
-  state.put(task,`request ${task} start: ${start},`);
+  startArr.push({task,start});
 }
 
-function requestEnd(task,start) {
-  if (state.has(task)) {
-  }
+function requestEnd(task,end) {
+    if (task === startArr[0].task){
+      console.log(`request${task} start: ${startArr[0].start}, end ${end}`);
+      startArr.shift();
+      while (true){
+        let index  = endArr.findIndex(e=>e.task===startArr[0].task);
+        if (index !== -1){
+          let task = endArr.splice(index,1);
+          console.log(`request${task[0].task} start: ${startArr[0].start}, end ${task[0].end}`);
+          startArr.shift();
+        } else {
+          break;
+        }
+      }
+    } else {
+      endArr.push({task:task,end:end});
+    }
 }
+requestStart(2, 1);
+requestStart(3, 2);
+requestEnd(3, 4);
+requestEnd(2, 5);
--- web/instanceof.js
+++ web/instanceof.js
@@ -8,12 +8,15 @@ function Ninja(){}
 Ninja.prototype = new Person();
 const ninja = new Ninja();
 
-console.log(ninja instanceof Ninja)
-console.log(ninja instanceof Person)
+console.log(ninja instanceof Ninja);  //true
+console.log(ninja instanceof Person);  //true
+console.log(ninja instanceof Object);  //true
+console.log(ninja instanceof Function); //false
+
 
 Ninja.prototype = {};
 
 const ninja2 = new Ninja();
 
-console.log(ninja instanceof Ninja);
-console.log(ninja2 instanceof Ninja);
+console.log(ninja instanceof Ninja); // false
+console.log(ninja2 instanceof Ninja); //true
