0b8ba3c6bf4f2ddbd93256d7370ea9fb7f7fb9ff
Yongzhao <yongzhao@geniusplaza.com>
2020-03-08 17:59:18 -0400

    Add changes

---
+++ lc/amazon/HitCounter.js
@@ -0,0 +1,77 @@
+/*
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+
+Each function accepts a timestamp parameter (in seconds granularity) and you may assume that calls are being made to the system in chronological order (ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+It is possible that several hits arrive roughly at the same time.
+
+Example:
+
+HitCounter counter = new HitCounter();
+
+// hit at timestamp 1.
+counter.hit(1);
+
+// hit at timestamp 2.
+counter.hit(2);
+
+// hit at timestamp 3.
+counter.hit(3);
+
+// get hits at timestamp 4, should return 3.
+counter.getHits(4);
+
+// hit at timestamp 300.
+counter.hit(300);
+
+// get hits at timestamp 300, should return 4.
+counter.getHits(300);
+
+// get hits at timestamp 301, should return 3.
+counter.getHits(301);
+
+Follow up:
+What if the number of hits per second could be very large? Does your design scale?
+
+ */
+
+/**
+ * Initialize your data structure here.
+ */
+var HitCounter = function() {
+  this.arr = [];
+
+};
+
+/**
+ * Record a hit.
+ @param timestamp - The current timestamp (in seconds granularity).
+ * @param {number} timestamp
+ * @return {void}
+ */
+HitCounter.prototype.hit = function(timestamp) {
+  this.arr[timestamp] = this.arr[timestamp] ? this.arr[timestamp] + 1 : 1;
+  this.total ++;
+};
+
+/**
+ * Return the number of hits in the past 5 minutes.
+ @param timestamp - The current timestamp (in seconds granularity).
+ * @param {number} timestamp
+ * @return {number}
+ */
+HitCounter.prototype.getHits = function(timestamp) {
+  let s = timestamp < 300 ? 0 : timestamp - 300;
+  let total = 0;
+  for (s = s+1; s <= timestamp; s ++){
+    total += this.arr[s] ? this.arr[s] : 0;
+  }
+  return total;
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * var obj = new HitCounter()
+ * obj.hit(timestamp)
+ * var param_2 = obj.getHits(timestamp)
+ */
---
+++ lc/amazon/MovingAverage.js
@@ -0,0 +1,48 @@
+/**
+ * Initialize your data structure here.
+ * @param {number} size
+ */
+var MovingAverage = function(size) {
+  this.arr = [];
+  this.currentSize = 0;
+  this.size = size;
+  this.total = 0;
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+MovingAverage.prototype.next = function(val) {
+  if(this.currentSize === this.size){
+    let value = this.arr.shift();
+    this.total = this.total - value + val;
+    this.arr.push(val);
+  }else{
+    this.currentSize ++;
+    this.total += val;
+    this.arr.push(val);
+  }
+  return this.total/this.currentSize;
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * var obj = new MovingAverage(size)
+ * var param_1 = obj.next(val)
+ */
+
+var obj = new MovingAverage(3);
+param_1 = obj.next(1);
+param_1 = obj.next(10);
+param_1 = obj.next(3);
+param_1 = obj.next(5);
+console.log(param_1);
+
+let mv = new MovingAverage(1);
+console.log(mv.next(4));
+console.log(mv.next(0));
+
+let arr = new Array(3);
+arr.push(1);
+console.log(arr.shift())
